6bc979827da370ef7b832b7ea1c6d5689767fdfb
Pablo Ferreras <pf@imunixx.de>
2023-05-04 13:53:45 +0200

    All data validated

--- front-end/src/app/components/movements/movements.component.ts
+++ front-end/src/app/components/movements/movements.component.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Movement } from 'src/app/domain/movement';
 import { Profile } from 'src/app/domain/profile';
 import { MovementService } from 'src/app/services/movement.service';
@@ -22,21 +22,42 @@ export class MovementsComponent {
   public movements !: Movement[];
   public matMovements !: MatTableDataSource<Movement>;
   public addMovement !: Movement;
+  public addMovementForm !: FormGroup;
   public editMovement ?: Movement;
   public deleteMovement ?: Movement;
   public profileDataLoaded : boolean = false;
+  public movementsDataLoaded : boolean = false;
   public isIncome !: boolean;
+  public balance : Decimal = new Decimal(0);
+  public submitted = false;
   displayedColumns: string[] = ['Concept', 'Description', 'Amount', 'Date'];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private profileService: ProfileService, private movementService : MovementService){
-    this.getSelectedProfile();
-    this.getMovements();
+  constructor(private formBuilder: FormBuilder, private profileService: ProfileService, private movementService : MovementService){
+
   }
 
   ngOnInit(){
+    this.getSelectedProfile();
+    this.getMovements();
+    this.getBalance();
 
+    this.addMovementForm = this.formBuilder.group({
+      concept:['', [
+        Validators.required
+      ]],
+      description:['', [
+        Validators.maxLength(200)
+      ]],
+      amount:['', [
+        Validators.required,
+        Validators.pattern(/^\d+(\.\d{1,2})?$/)
+      ]],
+      date: ['', [
+        Validators.required
+      ]]
+    })
   }
 
   getSelectedProfile() {
@@ -54,6 +75,7 @@ export class MovementsComponent {
   }
 
   public async getMovements() : Promise<void>{
+    this.movementsDataLoaded = false;
     await this.waitForProfileDataLoaded();
     this.movementService.getProfileMovements(this.selectedProfile.id).subscribe(
       (response: Movement[]) => {
@@ -62,17 +84,29 @@ export class MovementsComponent {
         this.matMovements.paginator = this.paginator;
         this.matMovements.sort = this.sort;
         console.log(this.movements);
+        this.movementsDataLoaded = true;
       },
       (error : HttpErrorResponse) => {
         alert(error.message);
       }
-    )
-    
+    )    
   }
 
-  public onAddMovement(addMovementForm : NgForm): void {
+  public async getBalance() {
+    this.balance = new Decimal(0);
+    await this.waitForMovementsDataLoaded();
+    this.movements.forEach(movement => {
+      this.balance = this.balance.plus(new Decimal(movement.amount));
+    });
+  }
+
+  public onAddMovement() {
+    this.submitted = true;
+    if(this.addMovementForm.invalid){
+      return
+    }
     document.getElementById('add-movement-form')!.click();
-    this.addMovement = addMovementForm.value;
+    this.addMovement = this.addMovementForm.value;
     this.addMovement.profile = this.selectedProfile;
     if(this.isIncome === false){
       const originalAmount = new Decimal(this.addMovement.amount);
@@ -81,14 +115,17 @@ export class MovementsComponent {
     this.movementService.addMovement(this.addMovement).subscribe(
       (response : Movement) => {
         console.log(response);
-        addMovementForm.reset();
+        this.addMovementForm.reset();
         this.getMovements();
+        this.balance = this.balance.plus(new Decimal(response.amount));
+        this.submitted = false;
       },
       (error : HttpErrorResponse) => {
         alert(error.message);
-        addMovementForm.reset();
+        this.addMovementForm.reset();
+        this.submitted = false;
       }
-    ) 
+    )
   }
 
   public onUpdateMovement(movement : Movement) : void {
@@ -96,19 +133,19 @@ export class MovementsComponent {
     this.movementService.updateMovement(movement).subscribe(
       (response : Movement) => {
         console.log(response);
-        this.getMovements();
       },
       (error : HttpErrorResponse) => {
         alert(error.message);
       }
     )
+    this.ngOnInit();
   }
 
   public onDeleteMovement(movementId : number) : void {
     this.movementService.deleteMovement(movementId).subscribe(
       (response : void) => {
         console.log(response);
-        this.getMovements();
+        this.ngOnInit();
       },
       (error : HttpErrorResponse) => {
         alert(error.message);
@@ -179,10 +216,26 @@ export class MovementsComponent {
     button.click();
   }
 
+  resetAddMovementForm() {
+    this.addMovementForm.reset();
+    this.submitted = false;
+  }
+
   waitForProfileDataLoaded(): Promise<void> {
     return new Promise(resolve => {
       const intervalId = setInterval(() => {
         if (this.profileDataLoaded) {
+          clearInterval(intervalId);
+          resolve();
+        }
+      }, 100);
+    });
+  }
+
+  waitForMovementsDataLoaded(): Promise<void> {
+    return new Promise(resolve => {
+      const intervalId = setInterval(() => {
+        if (this.movementsDataLoaded) {
           clearInterval(intervalId);
           resolve();
         }
